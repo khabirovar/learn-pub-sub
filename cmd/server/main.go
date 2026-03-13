356c38a4d31e5c780ed6a97b2fec90dc89bab490
Aidar Khabirov <khabirov.ar1990@gmail.com>
2026-03-13 15:21:31 +0500

    Add simple connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	rabbitConnString := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(rabbitConnString)
+	if err != nil {
+		log.Fatalf("Couldn't connect to rabbitmq %v\n",err)
+	}
+	defer conn.Close()
+	fmt.Println("Connection was successful...")
+
+	// wait for signal ctrl + c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Program is shuting down...")
 }
