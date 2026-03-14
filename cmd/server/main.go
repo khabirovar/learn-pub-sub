6da7da53ed7a8c72237cc8181d4f3cd960434cd9
Aidar Khabirov <khabirov.ar1990@gmail.com>
2026-03-14 10:09:50 +0500

    Publish simple message

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -19,9 +21,20 @@ func main() {
 	defer conn.Close()
 	fmt.Println("Connection was successful...")
 
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Couldn't create channel to rabbitmq: %v\n", err)
+	}
+	err = pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+	if err != nil {
+		log.Fatalf("Publishing error: %v", err)
+	}
+
 	// wait for signal ctrl + c
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
 	<-signalChan
 	fmt.Println("Program is shuting down...")
+
+
 }
