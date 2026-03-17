1278eab0a744dbe94c06ff711b8f6ebbb03d8e60
Aidar Khabirov <khabirov.ar1990@gmail.com>
2026-03-17 13:04:57 +0500

    Implement simple repl on server side

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -16,7 +15,7 @@ func main() {
 	rabbitConnString := "amqp://guest:guest@localhost:5672/"
 	conn, err := amqp.Dial(rabbitConnString)
 	if err != nil {
-		log.Fatalf("Couldn't connect to rabbitmq %v\n",err)
+		log.Fatalf("Couldn't connect to rabbitmq %v\n", err)
 	}
 	defer conn.Close()
 	fmt.Println("Connection was successful...")
@@ -25,16 +24,31 @@ func main() {
 	if err != nil {
 		log.Fatalf("Couldn't create channel to rabbitmq: %v\n", err)
 	}
-	err = pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
-	if err != nil {
-		log.Fatalf("Publishing error: %v", err)
+
+	gamelogic.PrintServerHelp()
+	for {
+		words := gamelogic.GetInput()
+		if len(words) < 1 {
+			continue
+		}
+
+		if words[0] == "pause" {
+			fmt.Println("Sending a pause message...")
+			err = pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+			if err != nil {
+				log.Fatalf("Publishing error: %v", err)
+			}
+		} else if words[0] == "resume" {
+			fmt.Println("Sending a resume message...")
+			err = pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: false})
+			if err != nil {
+				log.Fatalf("Publishing error: %v", err)
+			}
+		} else if words[0] == "quit" {
+			fmt.Println("Exiting...")
+			break
+		} else {
+			fmt.Println("Unknown command")
+		}
 	}
-
-	// wait for signal ctrl + c
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
-	fmt.Println("Program is shuting down...")
-
-
 }
